b05dfea3e61f80c82c4b845d1c7e03b2b8ec54c1
Shinx99 <nguyennam.zarakhan@gmail.com>
2025-10-02 10:53:21 +0700

    ps43652 - NguyenGiangNam - Slice 4 - First commit

---
+++ src/main/java/edu/poly/slice4/Slice4Application.java
@@ -0,0 +1,13 @@
+package edu.poly.slice4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slice4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slice4Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/poly/slice4/Slice4ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.poly.slice4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slice4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
